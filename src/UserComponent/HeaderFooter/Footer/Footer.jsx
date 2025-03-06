3197633471fb410bf959b70aeb43b6625a7bbd43
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-03-06 15:48:38 +0700

    Fix Header Footer

--- src/UserComponent/HeaderFooter/Footer/Footer.jsx
+++ src/UserComponent/HeaderFooter/Footer/Footer.jsx
@@ -1,41 +1,28 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
 
-    const [STOREs, setSTOREs] = useState(null);
-    const [TYPEs, setTYPEs] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
     return (
-        <div className='user-footer'>
-            <div className='footer-container'>
+        <div className='footer-container'>
+            <section className='section-1'>
                 <div className='footer-brand'>
-                    <h3><b>InnoSpace</b></h3>
-                    <p>InnoSpace là hệ thống quản lý và đặt chỗ làm việc cá nhân, giúp người dùng dễ dàng tìm kiếm và đặt chỗ theo nhu cầu.</p>
+                    <h1>Panpa</h1>
+                    <p>Panpa is a website that hosts games and learning materials for the purpose of improving programming skills.</p>
                 </div>
                 <div className='footer-location'>
-                    <h3><b>Địa chỉ</b></h3>
-                    <p>info@innospace.com.vn</p>
-                    {STOREs && STOREs.map((store) => (
-                        <div key={store.id}>
-                            <p>+84{store.contact.substring(1)}</p>
-                            <p>{store.address}</p>
-                        </div>
-                    ))}
+                    <h1>Email</h1>
+                    <p>dngngchitriu2004@gmail.com</p>
+                    <p>trieuDNHSE180392@fpt.edu.vn</p>
                 </div>
                 <div className='footer-solution'>
-                    <h3><b>Giải pháp</b></h3>
-                    {TYPEs && TYPEs.map((type) => (
-                        <p key={type.id}>{type.name} {type.capacity} Người</p>
-                    ))}
+                    <h1>Contact</h1>
+                    <p>Viettel: ***8370030</p>
                 </div>
-            </div>
-            <div className='footer-end'>
-                <p>Copyright © 2024 Tactic</p>
-            </div>
+            </section>
+            <section className='section-2'>
+                <p>Copyright © 2025 Tactic</p>
+            </section>
         </div>
     )
 }
--- src/UserComponent/HeaderFooter/Header/GuestHeader.jsx
+++ src/UserComponent/HeaderFooter/Header/GuestHeader.jsx
@@ -1,23 +1,16 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import { AppBar, Drawer, IconButton, List, ListItem, ListItemText, Toolbar, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './GuestHeader.css';
 
 export default function GuestHeader() {
-
-    const [id, setId] = useState(null);
-    const UserId = localStorage.getItem('UserId');
-    useEffect(() => {
-        const UserIdInt = parseInt(UserId, 10);
-        setId(UserIdInt);
-    }, [UserId]);
 
     const [menuOpen, setMenuOpen] = useState(false);
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
-    
+
     const menuItems = [
         { text: 'Home', path: '/' },
         { text: 'About', path: '/about' },
@@ -30,9 +23,10 @@ export default function GuestHeader() {
 
     return (
         <div className='guestheader-container'>
-            <AppBar position="static" className='menu'>
+            <AppBar position='static' className='menu'>
                 <Toolbar>
-                    <Typography variant="h6" style={{ flexGrow: 1 }}>
+
+                    <Typography variant='h6' style={{ flexGrow: 1 }}>
                         <Link to='/' className='brand-logo'><b>Panpa</b></Link>
                     </Typography>
 
@@ -44,27 +38,23 @@ export default function GuestHeader() {
                     <Link to='/japanese' className='menu-item'><i className='fa-solid fa-torii-gate icon'></i> Japanese</Link>
                     <Link to='/signinsignup' className='menu-item'><i className='fas fa-sign-in-alt icon'></i> Sign In</Link>
 
-                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleMenu}>
+                    <IconButton edge='start' color='inherit' aria-label='menu' onClick={toggleMenu}>
                         <MenuIcon className='menu-icon' />
                     </IconButton>
 
                 </Toolbar>
             </AppBar>
 
-            <Drawer anchor="right" open={menuOpen} onClose={toggleMenu} className='drawer'>
+            <Drawer anchor='right' open={menuOpen} onClose={toggleMenu} className='drawer'>
                 <List className='drawer-list' style={{ height: '100%', backgroundColor: '#fcfcfc' }}>
-
                     {menuItems.map((item) => (
                         <ListItem key={item.text} component={Link} to={item.path} onClick={toggleMenu}>
                             <ListItemText primary={item.text}
                                 style={{ color: '#000000', fontFamily: 'Quicksand', fontWeight: 'bold' }} />
                         </ListItem>
                     ))}
-
                 </List>
             </Drawer>
-
-            <Outlet />
         </div>
     );
 }
