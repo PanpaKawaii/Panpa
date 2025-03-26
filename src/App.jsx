f5b88f047f2c8d16791222fa43d4110082939baa
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-03-26 23:40:45 +0700

    File Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import MainRoutes from './app/routes/MainRoutes.jsx'
 
-import Footer from './UserComponent/HeaderFooter/Footer/Footer.jsx'
-import GuestHeader from './UserComponent/HeaderFooter/Header/GuestHeader.jsx'
-
-import HomeContent from './UserComponent/Home/HomeContent.jsx'
-
-import ListGame from './UserComponent/Game/ListGame.jsx'
-import Minesweeper from './UserComponent/Game/Minesweeper.jsx'
-import WhatIsThePassword from './UserComponent/Game/WhatIsThePassword.jsx'
-import Caro from './UserComponent/Game/Caro.jsx'
-import Chess from './UserComponent/Game/Chess.jsx'
-import Tetris from './UserComponent/Game/Tetris.jsx'
-import SolveTheMaze from './UserComponent/Game/SolveTheMaze.jsx'
-import GenerateMaze from './UserComponent/Game/GenerateMaze.jsx'
-import TypePi from './UserComponent/Game/TypePi.jsx'
-import RandomCard from './UserComponent/Game/RandomCard.jsx'
-import ToDoList from './UserComponent/Game/ToDoList.jsx'
-import Object from './UserComponent/Game/Object.jsx'
-
-import Space from './UserComponent/Object/Space.jsx'
-
-import ListJapanese from './UserComponent/Japanese/ListJapanese.jsx'
-import JapaneseKanji from './UserComponent/Japanese/JapaneseKanji.jsx'
-import JapaneseVerb from './UserComponent/Japanese/JapaneseVerb.jsx'
-import ScrollToTop from './ScrollToTopComponent/ScrollToTop.js'
-
-function App() {
-
-  return (
-    <>
-      <ScrollToTop />
-      <GuestHeader />
-      <Routes>
-        <Route path='/' element={<HomeContent />} />
-
-        <Route path='game' element={<ListGame />} />
-        <Route path='game/minesweeper' element={<Minesweeper />} />
-        <Route path='game/whatisthepassword' element={<WhatIsThePassword />} />
-        <Route path='game/caro' element={<Caro />} />
-        <Route path='game/chess' element={<Chess />} />
-        <Route path='game/tetris' element={<Tetris />} />
-        <Route path='game/solvethemaze' element={<SolveTheMaze />} />
-        <Route path='game/generatemaze' element={<GenerateMaze />} />
-        <Route path='game/typepi' element={<TypePi />} />
-        <Route path='game/randomcard' element={<RandomCard />} />
-        <Route path='game/todolist' element={<ToDoList />} />
-        <Route path='game/object' element={<Object />} />
-
-        <Route path='space' element={<Space />} />
-
-        <Route path='japanese' element={<ListJapanese />} >
-          <Route path='kanji' element={<JapaneseKanji />} />
-          <Route path='verb' element={<JapaneseVerb />} />
-        </Route>
-
-      </Routes>
-      <Footer />
-    </>
-  )
+export default function App() {
+  return <MainRoutes />
 }
-
-export default App
---
+++ src/app/routes/MainRoutes.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import ScrollToTop from '../hooks/ScrollToTop/useScrollToTop.jsx'
+
+import Footer from '../layouts/Footer/Footer.jsx'
+import GuestHeader from '../layouts/Header/GuestHeader.jsx'
+
+import HomeContent from '../pages/Home/HomeContent.jsx'
+
+import Caro from '../pages/Game/Caro.jsx'
+import Chess from '../pages/Game/Chess.jsx'
+import GenerateMaze from '../pages/Game/GenerateMaze.jsx'
+import ListGame from '../pages/Game/ListGame.jsx'
+import Minesweeper from '../pages/Game/Minesweeper.jsx'
+import Object from '../pages/Game/Object.jsx'
+import RandomCard from '../pages/Game/RandomCard.jsx'
+import SolveTheMaze from '../pages/Game/SolveTheMaze.jsx'
+import Tetris from '../pages/Game/Tetris.jsx'
+import ToDoList from '../pages/Game/ToDoList.jsx'
+import TypePi from '../pages/Game/TypePi.jsx'
+import WhatIsThePassword from '../pages/Game/WhatIsThePassword.jsx'
+
+import Space from '../pages/Object/Space.jsx'
+
+import JapaneseKanji from '../pages/Japanese/JapaneseKanji.jsx'
+import JapaneseVerb from '../pages/Japanese/JapaneseVerb.jsx'
+import ListJapanese from '../pages/Japanese/ListJapanese.jsx'
+
+export default function MainRoutes() {
+    return (
+        <BrowserRouter>
+            <ScrollToTop />
+            <GuestHeader />
+            <Routes>
+                <Route path='/' element={<HomeContent />} />
+
+                <Route path='game' element={<ListGame />} />
+                <Route path='game/minesweeper' element={<Minesweeper />} />
+                <Route path='game/whatisthepassword' element={<WhatIsThePassword />} />
+                <Route path='game/caro' element={<Caro />} />
+                <Route path='game/chess' element={<Chess />} />
+                <Route path='game/tetris' element={<Tetris />} />
+                <Route path='game/solvethemaze' element={<SolveTheMaze />} />
+                <Route path='game/generatemaze' element={<GenerateMaze />} />
+                <Route path='game/typepi' element={<TypePi />} />
+                <Route path='game/randomcard' element={<RandomCard />} />
+                <Route path='game/todolist' element={<ToDoList />} />
+                <Route path='game/object' element={<Object />} />
+
+                <Route path='space' element={<Space />} />
+
+                <Route path='japanese' element={<ListJapanese />} >
+                    <Route path='kanji' element={<JapaneseKanji />} />
+                    <Route path='verb' element={<JapaneseVerb />} />
+                </Route>
+
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
+}
