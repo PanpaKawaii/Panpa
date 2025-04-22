f759df1d98970b74574569a2c21bc6dac0eb6864
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-04-22 10:58:36 +0700

    Fix Icosahedron

--- src/app/pages/Object/Icosahedron.jsx
+++ src/app/pages/Object/Icosahedron.jsx
@@ -2,30 +2,31 @@ import React from 'react';
 import './Icosahedron.css';
 
 export default function Icosahedron() {
+
+    const Distance = 75.577;
+    const Angle = -37.377;
+    const SubAngle = [0, 41.81, -138.19, 180];
+
     return (
         <>
             <div className='scene-icosahedron'>
                 <div className='icosahedron'>
-                    <div className='face f1'>Face1</div>
-                    <div className='face f2'>Face2</div>
-                    <div className='face f3'>Face3</div>
-                    <div className='face f4'>Face4</div>
-                    <div className='face f5'>Face5</div>
-                    <div className='face f6'>Face6</div>
-                    <div className='face f7'>Face7</div>
-                    <div className='face f8'>Face8</div>
-                    <div className='face f9'>Face9</div>
-                    <div className='face f10'>Face10</div>
-                    <div className='face f11'>Face11</div>
-                    <div className='face f12'>Face12</div>
-                    <div className='face f13'>Face13</div>
-                    <div className='face f14'>Face14</div>
-                    <div className='face f15'>Face15</div>
-                    <div className='face f16'>Face16</div>
-                    <div className='face f17'>Face17</div>
-                    <div className='face f18'>Face18</div>
-                    <div className='face f19'>Face19</div>
-                    <div className='face f20'>Face20</div>
+                    {[...Array(20)].map((_, index) => (
+                        <div
+                            key={index}
+                            className={`face f${index + 1}`}
+                            style={{
+                                background: `hsl(${Math.round(index * (360 / 20))}, 70%, 50%, 0.5)`,
+                                transform: `
+                                rotateZ(${(index <= 4 || index >= 15) ? (index % 5) * 72 : ((index >= 5 || index <= 14) ? (index % 5) * 72 + 36 : 0)}deg)
+                                rotateX(${(Math.floor(index / 5) === 0 || Math.floor(index / 5) === 3) ? SubAngle[Math.floor(index / 5)] + Angle : SubAngle[Math.floor(index / 5)] - Angle}deg)
+                                translateZ(${Distance}px)
+                                `,
+                            }}
+                        >
+                            Face{index + 1}
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
