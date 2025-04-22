85149886dc35ccfb1a1a8735bfba183ea1351300
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-04-22 08:21:51 +0700

    Add Object Space

--- src/app/pages/Object/Object.jsx
+++ src/app/pages/Object/Object.jsx
@@ -8,6 +8,7 @@ import Dodecahedron from './Dodecahedron.jsx';
 import Icosahedron from './Icosahedron.jsx';
 import Sphere from './Sphere.jsx';
 import Crown from './Crown.jsx';
+import Space from './Space.jsx';
 
 export default function Object() {
 
@@ -22,16 +23,24 @@ export default function Object() {
         <Icosahedron />,
         <Sphere />,
         <Crown x={Position[0] * 10} y={Position[1] * 10} z={Position[2] * 10} />,
+        <Space />,
     ];
 
     return (
         <div className='object-container'>
-            <div className='content'>
-                <button className='btn' onClick={() => setCount(p => p - 1)}>LEFT</button>
+            <div
+                className='content'
+                // style={{
+                //     transform: `rotateX(${Position[0] * 10}deg) rotateY(${Position[1] * 10}deg) rotateZ(${Position[2] * 10}deg)`,
+                // }}
+            >
                 {List[((Count % List.length) + List.length) % List.length]}
-                <button className='btn' onClick={() => setCount(p => p + 1)}>RIGHT</button>
             </div>
-            <form className='slider-form'>
+
+            <div className='slider-form'>
+                <button className='btn' onClick={() => setCount(p => p - 1)}>LEFT</button>
+                <button className='btn' onClick={() => setCount(p => p + 1)}>RIGHT</button>
+
                 <label>
                     <div>X-axis: {Position[0] * 10}°</div>
                     <input type='range' name='Xaxis' min='-36' max='36' value={Position[0]} onChange={(e) => { setPosition([e.target.value, Position[1], Position[2]]) }} />
@@ -46,7 +55,7 @@ export default function Object() {
                     <div>Z-axis: {Position[2] * 10}°</div>
                     <input type='range' name='Zaxis' min='-36' max='36' value={Position[2]} onChange={(e) => { setPosition([Position[0], Position[1], e.target.value]) }} />
                 </label>
-            </form>
+            </div>
         </div>
     );
 }
---
+++ src/app/pages/Object/Space.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Space.css';
+
+export default function Space() {
+    return (
+        <div className='object-space-container'>
+            <div className='icosahedron'>
+                <div className='face f1'>Face1</div>
+                <div className='face f2'>Face2</div>
+                <div className='face f3'>Face3</div>
+                <div className='face f4'>Face4</div>
+                <div className='face f5'>Face5</div>
+                <div className='face f6'>Face6</div>
+                <div className='face f7'>Face7</div>
+                <div className='face f8'>Face8</div>
+                <div className='face f9'>Face9</div>
+                <div className='face f10'>Face10</div>
+                <div className='face f11'>Face11</div>
+                <div className='face f12'>Face12</div>
+                <div className='face f13'>Face13</div>
+                <div className='face f14'>Face14</div>
+                <div className='face f15'>Face15</div>
+                <div className='face f16'>Face16</div>
+                <div className='face f17'>Face17</div>
+                <div className='face f18'>Face18</div>
+                <div className='face f19'>Face19</div>
+                <div className='face f20'>Face20</div>
+            </div>
+            <div className='dodecahedron'>
+                <div className='face f1'>Face1</div>
+                <div className='face f2'>Face2</div>
+                <div className='face f3'>Face3</div>
+                <div className='face f4'>Face4</div>
+                <div className='face f5'>Face5</div>
+                <div className='face f6'>Face6</div>
+                <div className='face f7'>Face7</div>
+                <div className='face f8'>Face8</div>
+                <div className='face f9'>Face9</div>
+                <div className='face f10'>Face10</div>
+                <div className='face f11'>Face11</div>
+                <div className='face f12'>Face12</div>
+            </div>
+        </div>
+    )
+}
