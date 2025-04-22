791bf25b7da8a6685904d7c11aa157955895a43f
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-04-22 10:24:07 +0700

    Fix Dodecahedron

--- src/app/pages/Object/Dodecahedron.jsx
+++ src/app/pages/Object/Dodecahedron.jsx
@@ -2,22 +2,33 @@ import React from 'react';
 import './Dodecahedron.css';
 
 export default function Dodecahedron() {
+
+    const Distance = 68.7;
+    const SubDistance = -5.02;
+    const Angle = -116.565;
+
     return (
         <>
             <div className='scene-dodecahedron'>
                 <div className='dodecahedron'>
-                    <div className='face f1'>Face1</div>
-                    <div className='face f2'>Face2</div>
-                    <div className='face f3'>Face3</div>
-                    <div className='face f4'>Face4</div>
-                    <div className='face f5'>Face5</div>
-                    <div className='face f6'>Face6</div>
-                    <div className='face f7'>Face7</div>
-                    <div className='face f8'>Face8</div>
-                    <div className='face f9'>Face9</div>
-                    <div className='face f10'>Face10</div>
-                    <div className='face f11'>Face11</div>
-                    <div className='face f12'>Face12</div>
+                    {[...Array(12)].map((_, index) => (
+                        <div
+                            key={index}
+                            className={`face f${index + 1}`}
+                            style={{
+                                background: `hsl(${Math.round(index * (360 / 12))}, 70%, 50%, 0.5)`,
+                                transform: `
+                                rotateZ(${index === 6 ? 180 : index * 72}deg)
+                                rotateX(${(index !== 0 && index !== 6) ? (index < 6 ? Angle : 180 + Angle) : 0}deg)
+                                rotateY(${index === 0 ? 180 : 0}deg)
+                                translateY(${SubDistance}px)
+                                translateZ(${Distance}px)
+                                `,
+                            }}
+                        >
+                            Face{index + 1}
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
