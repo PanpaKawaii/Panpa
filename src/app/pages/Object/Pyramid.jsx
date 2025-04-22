a41f15946076444bfb1ab6fe0e85eb1fad7a32b8
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-04-22 12:23:09 +0700

    Fix Pyramid

--- src/app/pages/Object/Pyramid.jsx
+++ src/app/pages/Object/Pyramid.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import './Pyramid.css';
 
 export default function Pyramid() {
+
+    const Distance = 40.824;
+    const Angle = -70.529;
+
     return (
         <>
             <div className='scene-pyramid'>
                 <div className='pyramid'>
-                    <div className='face bottom'>Yellow</div>
-                    <div className='face front'>Red</div>
-                    <div className='face left'>Green</div>
-                    <div className='face right'>Blue</div>
+                    {[...Array(4)].map((_, index) => (
+                        <div
+                            key={index}
+                            className={`face f${index + 1}`}
+                            style={{
+                                background: `hsl(${Math.round(index * (360 / 4))}, 70%, 50%, 0.5)`,
+                                transform: `
+                                rotateZ(${index !== 0 ? index * 120 : 0}deg)
+                                rotateX(${index !== 0 ? Angle : 0}deg)
+                                rotateY(${index === 0 ? 180 : 0}deg)
+                                translateZ(${Distance}px)
+                                `,
+                            }}
+                        >
+                            Face{index + 1}
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
